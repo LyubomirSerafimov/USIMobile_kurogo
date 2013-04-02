ec73ab95c1545f94b701ea66f447a698950357b0
Goran Josic <goran.josic@usi.ch>
2013-04-02 09:58:36 +0200

    Moved the USIsportSOAP config vars to site.ini

--- lib/USIsportSOAP.php
+++ lib/USIsportSOAP.php
@@ -5,11 +5,11 @@ class USIsportSOAP {
 	public function checkMembership($username, $password) { 
 		// check function parameters
 		if(empty($username)) {
-			return $this-error(0, 'Missing username');
+			return $this->error(0, 'Missing username');
 		}
 
 		if(empty($password)) {
-			return $this-error(1, 'Missing password');
+			return $this->error(1, 'Missing password');
 		}
 
 		$params = array('netidlogin' => $username, 'password' => $password);
@@ -20,15 +20,15 @@ class USIsportSOAP {
 	public function checkSubscription($username, $password, $activity) { 
 		// check function parameters
 		if(empty($username)) {
-			return $this-error(0, 'Missing username');
+			return $this->error(0, 'Missing username');
 		}
 
 		if(empty($password)) {
-			return $this-error(1, 'Missing password');
+			return $this->error(1, 'Missing password');
 		}
 
 		if(empty($activity)) {
-			return $this-error(2, 'Missing activity');
+			return $this->error(2, 'Missing activity');
 		}
 
 		$params = array('netidlogin' => $username, 'password' => $password, 'idAttivita' => $activity);
@@ -39,15 +39,15 @@ class USIsportSOAP {
 	public function subscribe($username, $password, $activity) { 
 		// check function parameters
 		if(empty($username)) {
-			return $this-error(0, 'Missing username');
+			return $this->error(0, 'Missing username');
 		}
 
 		if(empty($password)) {
-			return $this-error(1, 'Missing password');
+			return $this->error(1, 'Missing password');
 		}
 
 		if(empty($activity)) {
-			return $this-error(2, 'Missing activity');
+			return $this->error(2, 'Missing activity');
 		}
 
 		$params = array('netidlogin' => $username, 'password' => $password, 'idAttivita' => $activity);
@@ -58,35 +58,36 @@ class USIsportSOAP {
 	public function unsubscribe($username, $password, $activity) { 
 		// check function parameters
 		if(empty($username)) {
-			return $this-error(0, 'Missing username');
+			return $this->error(0, 'Missing username');
 		}
 
 		if(empty($password)) {
-			return $this-error(1, 'Missing password');
+			return $this->error(1, 'Missing password');
 		}
 
 		if(empty($activity)) {
-			return $this-error(2, 'Missing activity');
+			return $this->error(2, 'Missing activity');
 		}
 
 		$params = array('netidlogin' => $username, 'password' => $password, 'idAttivita' => $activity);
-		return $this->soapRequest($this->config->getVar('UNSUBSCRIBE_METHOD', 'usi_sport'), $params);
+		$config = Kurogo::siteConfig();
+		return $this->soapRequest($config->getVar('UNSUBSCRIBE_METHOD', 'usi_sport'), $params);
 	}
 
 	public function soapRequest($method, $params) {
 		$config = Kurogo::siteConfig();
-		$soapClient = new SoapClient($config->getVar('WDSL', 'usi_sport'));
+		$soapClient = new SoapClient($config->getVar('WDSL', 'usi_sport'), array('cache_wsdl' => WSDL_CACHE_NONE));
                    
         try {
             $response = $soapClient->__call($method, array($params));
         } catch (SoapFault $fault) {
-			$message = 'CODE: ' . $fault->faultcode . ' ' . $fault->faultstring;
-			return $this-error(3, $message);
+			//$message = 'CODE: ' . $fault->faultcode . ' ' . $fault->faultstring;
+			return $this->error($fault->faultcode, $fault->faultstring);
         }
 
 		$result = $response->{$method.'Result'};
 		if(!empty($result->flagErrore)) {
-			return $this->error(4, $result->MessaggioErrore);
+			return $this->error($result->CodiceErrore, $result->MessaggioErrore);
 		} else if(isset($result->Oggetto)) {
 			if($result->Oggetto == 'True') {
 				return true;
